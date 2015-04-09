d3208335b1d5adeedd29474191e618a023e589c6
Marcie Van Auken <marcievanauken@gmail.com>
2015-04-09 18:56:00 -0400

    working on hw

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	console.log('My page loaded!');
+	console.log('My page loaded!'); //to make sure js file is linked
 
     //function for buttonsArgue div -- the buttons do not get a long
     $("#buttonOne").click(function(){
@@ -15,7 +15,21 @@ $(document).ready(function(){
 
 
 
+    //function for dontHover div -- it's an angry yellow square
+	 $(".dontHover").mouseover(function(){
+        $(".hover").show();
+        $(".noHover").hide();
+    });
 
+	 $(".dontHover").mouseout(function(){
+        $(".noHover").show();
+        $(".hover").hide();
+    });
+
+	 //trying to do the above with native javascript
+	function onmouseover() {
+		document.getElementById('#noHover').innerHTML = 'Hey, I told you not to hover over me!';
+	}
 
 
 });
